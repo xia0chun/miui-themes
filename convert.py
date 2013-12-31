a40f61a9cb87cf0675cb757834cc3d8f211a376c
xia0chun <xia0chun@126.com>
2013-12-31 15:08:24 +0800

    修改

--- convert.py
+++ convert.py
@@ -8,10 +8,11 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 
 #连接数据库
-con = sqlite3.connect("/home/xiaochun/MIUI/Source/db/mimi.db")
+con = sqlite3.connect("/home/xiaochun/Project/source/mimi.db")
 cur = con.cursor()
 
-for i in range(1,10+1):
+for i in range(2300,2362+1):
+	#先根据id检索出主题名称和PageUrl
 	sql = "select 名称,PageUrl from content where id = " + str(i)
 	cur.execute(sql)
 	res = cur.fetchone()
@@ -22,13 +23,11 @@ for i in range(1,10+1):
 	html_parser = HTMLParser.HTMLParser()
 	trueThemeName = html_parser.unescape(themeName)
 	
-	print trueThemeName
-	#print pageUrl
-	
-	#根据名称重新查询PageUrl
-	sql = "select id from content where PageUrl = '" + pageUrl + "'"
+	#根据PageUrl替换名称
+	sql = 'update content set 名称 = "' + trueThemeName + '" where PageUrl = "' + pageUrl + '"'
 	cur.execute(sql)
-	res = cur.fetchall()
-	print str(len(res))
+	con.commit()
+	
+	print str(i) + ". " + trueThemeName
 	
 	i = i + 1
