b551977dc0e1a365b5534b68e4023a41b18f138f
xia0chun <xia0chun@126.com>
2014-01-01 20:59:47 +0800

    修改

--- down.py
+++ down.py
@@ -3,23 +3,42 @@ import sqlite3
 import os
 import urllib2
 
+#将系统默认编码设置为utf-8
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-con = sqlite3.connect("url.db")
+#连接数据库
+con = sqlite3.connect("mimi.db")
 cur = con.cursor()
 
-for i in range(351,1500+1):
-	sql = "select PageUrl from content where id = " + str(i)
+for i in range(1,2+1):
+	
+	#从数据库中检索出更新时间和Pageurl
+	sql = "select 更新时间,PageUrl from content where id = " + str(i)
 	cur.execute(sql)
 	res = cur.fetchone()
-	pageUrl = res[0]
+	updateTime = res[0]
+	pageUrl = res[1]
+	
+	#根据PageUrl构建主题文件的ID和下载链接
+	themeID = pageUrl.split('/')[-1]
 	downUrl = pageUrl.replace("detail","download")
 	
-	r = urllib2.urlopen(downUrl.rstrip())
-	file_name = urllib2.unquote(r.geturl()).decode('utf8').split('/')[-1]
-	op = "wget -O ~/MIUI/Themes/" + str(i) + "." + file_name + " " + downUrl
-	os.system(op)
-	os.system("sleep 3")
+	#构建本地存放路径
+	localDir = "/home/xiaochun/MIUI/Download/" + themeID
+	
+	#判断本地路径是否存在，如果不存在则建立该路径
+	if not os.path.exists(localDir):
+		op = "mkdir " + localDir
+		os.system(op)
+	
+	#构建本地文件名，以更新时间为文件名
+	localName = updateTime + ".mtz"
+	
+	#判断本地文件是否存在，如果不存在则下载该文件
+	if not os.path.isfile(localDir + localName):
+		op = "wget -O " + localDir + "/" + localName + " " + downUrl
+		os.system(op)
+	
 	i = i + 1
