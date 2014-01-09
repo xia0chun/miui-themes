b299f85e65f3b09fc9b978e4dd538e3b28c6e9d0
xia0chun <xia0chun@126.com>
2014-01-09 16:41:58 +0800

    增加解析

--- xia0mi.py
+++ xia0mi.py
@@ -2,6 +2,7 @@
 import sqlite3
 import os
 import urllib2
+import re
 from bs4 import BeautifulSoup
 
 #将系统默认编码设置为utf-8
@@ -9,13 +10,32 @@ import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-urllist = [];
+urlList = [];
+thumbList = [];
+
+urlPattern = re.compile(r'href=.+?"')
+namePattern = re.compile(r'title=.+?"')
+imgPattern = re.compile(r'data-src=.+?"')
 
 for i in range(1,83+1):
 	url = "http://zhuti.xiaomi.com/compound?page=" + str(i) + "&sort=New"
-	urllist.append(url)
+	urlList.append(url)
 	
 soup = BeautifulSoup(urllib2.urlopen("http://zhuti.xiaomi.com/compound?page=1&sort=New").read())
 
 for thumb in soup.findAll("div", {"class" : "thumb"}):
-	print thumb
+	#解析URL链接
+	urlMatch = re.findall(urlPattern,str(thumb))
+	print urlMatch[0]
+	
+	#解析主题名称
+	nameMatch = re.findall(namePattern,str(thumb))
+	print nameMatch[0]
+	
+	#解析缩略图链接
+	imgMatch = re.findall(imgPattern,str(thumb))
+	print imgMatch[0]
+	
+	thumbList.append(thumb)
+
+
